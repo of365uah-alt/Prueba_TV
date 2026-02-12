c4762e3221cf6948d45524aa1a4051696a4b5c70
Daniel Márquez <daniel.marquez2@educa.madrid.org>
2026-02-12 15:48:07 +0100

    A toma po culo

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -1,38 +1,5 @@
 # Hola, soy un comentario con derechos. Si me borras procedere a denunciarte y a reclamar una indemnizacion por daños y prejuicios.
 
-print("Hola mundo")
 
-import random
-def metodo(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-    return 0
-
-def metodo2(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo3(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo4(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo5(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print('polnito')
 
 # sigma
-
-                        
-
-metodo("Hola")
