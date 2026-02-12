c9e4b507b2512786919374ab1b4b501262466fbb
Razielykain <italo.pisciotti@edu.uah.es>
2026-02-12 15:52:32 +0100

    prueba del polnito

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -1,4 +1,8 @@
 # Hola, soy un comentario con derechos. Si me borras procedere a denunciarte y a reclamar una indemnizacion por daños y prejuicios.
-# Esta clase esta vacia.
 
 print("Hola mundo")
+
+def terraseñal(a:int) -> None:
+    print(a)
+    return None
+print(terraseñal(67))
