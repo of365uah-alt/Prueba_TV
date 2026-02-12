8c2e00dc5a46ebd09d33fedf393fbd461a82b46f
Daniel Márquez <daniel.marquez2@educa.madrid.org>
2026-02-12 16:29:27 +0100

    Una pequeña palabra

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -1,9 +1,9 @@
 # Hola, soy un comentario con derechos. Si me borras procedere a denunciarte y a reclamar una indemnizacion por daños y prejuicios.
 
-print("Hola NEGRAS")
+print("Hola Blanques")
 def main():
     casa=True
     while casa==True:
-        print("dejen de ir a puticlubs, por favor")
+        print("dejen de ir a tiendas de zapatos (referencia GTA V), por favor")
 
 main()
