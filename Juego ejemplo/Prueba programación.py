f049a646d9db0ca11b01652979ee561b5091b4e0
Daniel Márquez <daniel.marquez2@educa.madrid.org>
2026-02-12 15:49:48 +0100

    Otro coment

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -1,5 +1,4 @@
 # Hola, soy un comentario con derechos. Si me borras procedere a denunciarte y a reclamar una indemnizacion por daños y prejuicios.
+# Esta clase esta vacia.
 
 print("Hola mundo")
-
-import random
