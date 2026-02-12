02c17c919bd68dfba7f3724ec78c3b2638fd8f3f
of365uah-alt <of365uah@gmail.com>
2026-02-12 15:30:47 +0100

    añadir metodo2

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -8,4 +8,11 @@ def metodo(s: str) -> int:
         print(tabla)
     return 0
 
+def metodo2(s: str) -> int:
+    tabla = []
+    for i in range(10):
+        tabla.append(random.randint(1,10))
+        print(tabla)
+
+
 metodo("Hola")
