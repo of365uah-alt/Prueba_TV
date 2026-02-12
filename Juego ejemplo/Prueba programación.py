2544b2bba0939fd2775d19251e7f9f016cd70e26
of365uah-alt <of365uah@gmail.com>
2026-02-12 15:46:16 +0100

    Reiniciar todo!

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -3,36 +3,3 @@
 print("Hola mundo")
 
 import random
-def metodo(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-    return 0
-
-def metodo2(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo3(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo4(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print(tabla)
-def metodo5(s: str) -> int:
-    tabla = []
-    for i in range(10):
-        tabla.append(random.randint(1,10))
-        print('polnito')
-
-# sigma
-
-                        
-
-metodo("Hola")
