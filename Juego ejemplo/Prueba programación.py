eeb15995702d57d880486643b08c1484f2881df1
Daniel Márquez <daniel.marquez2@educa.madrid.org>
2026-02-12 16:16:05 +0100

    Parry Hotter

--- Juego ejemplo/Prueba programación.py
+++ Juego ejemplo/Prueba programación.py
@@ -1,6 +1,6 @@
 # Hola, soy un comentario con derechos. Si me borras procedere a denunciarte y a reclamar una indemnizacion por daños y prejuicios.
 
 <<<<<<< HEAD
-print("Hola mundo")
+print("Hola Africa")
 def main():
     print("dejen de hacer branches, por favor")
